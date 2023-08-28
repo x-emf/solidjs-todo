69f4d4fc05c9723c822a3bdb642fd58d67528b53
X-EMF <136609943+x-emf@users.noreply.github.com>
2023-08-28 09:56:08 -0400

    add interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { For, type Component, createSignal } from 'solid-js';
 
 const App: Component = () => {
-  const [todos, setTodos] = createSignal(["a", "b"]);
+  const [todos, setTodos] = createSignal<Task[]>([]);
+  const addTodo = (newTodo: Task) => setTodos(xs => [...xs, newTodo]);
+  addTodo({name: "Invent the universe"});
+  addTodo({name: "Make apple pie from scratch"});
   return (
     <> 
       <h3>Your todos:</h3>
       <ul>
         <For each={todos()}>{item =>
-          <li>{item}</li>
+          <li>{item.name}</li>
         }</For>
       </ul>
     </>
